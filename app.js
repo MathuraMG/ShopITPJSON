abbf10029d8d3815d7ea9366c3fbc92ef6b0cc3a
mathuramg <mmg9291@gmail.com>
2016-04-13 10:22:53 -0400

    prep for deploy

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/login', function (req,res){
 
 
 // Start our server
-var server = app.listen(4000, function () {
+var server = app.listen(process.env.PORT || 4000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('app listening at http://%s:%s', host, port);
